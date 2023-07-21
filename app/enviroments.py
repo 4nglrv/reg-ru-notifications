87ee28847dfe2a69ac51a3198392c63e05cf380f
Oleg Angulyarov <4nglrv@gmail.com>
2023-07-21 21:01:46 +0300

    Fix

--- app/enviroments.py
+++ app/enviroments.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get environment variables
-REG_RU_API_KEY = os.getenv('REG_RU_API_KEY')
-TG_BOT_API_KEY = os.getenv('TG_BOT_API_KEY')
-DATA_FILENAME = os.getenv('DATA_FILENAME', default='data.txt')
-SCHEDULE_TIME = os.getenv('SCHEDULE_TIME', default='18:00')
-REGION = os.getenv('REGION', default='Europe/Moscow')
+REG_RU_API_KEY = os.environ.get('REG_RU_API_KEY')
+TG_BOT_API_KEY = os.environ.get('TG_BOT_API_KEY')
+DATA_FILENAME = os.environ.get('DATA_FILENAME', default='data.txt')
+SCHEDULE_TIME = os.environ.get('SCHEDULE_TIME', default='18:00')
+REGION = os.environ.get('REGION', default='Europe/Moscow')
--- app/main.py
+++ app/main.py
@@ -12,29 +12,28 @@ chats = Chats()
 bot = telebot.TeleBot(TG_BOT_API_KEY)
 
 
+def requestToReg():
+    response = requests.get('https://api.cloudvps.reg.ru/v1/balance_data', headers={
+        "Authorization": REG_RU_API_KEY
+    })
+    data = json.loads(response.content)
+    print(f'Response: {data}')
+
+    balance = data["balance_data"]["balance"]
+    days_left = data["balance_data"]["days_left"]
+    monthly_cost = data["balance_data"]["monthly_cost"]
+
+    message = f'Облачный счет составляет *{balance} ₽*\n' \
+              f'Этого хватит на *{days_left} дн.*\n' \
+              f'Расход средств и прогноз потребления: *{monthly_cost} ₽/месяц*'
+
+    if len(chats.state) and days_left <= 5:
+        for chat in chats.state:
+            bot.send_message(chat, message, parse_mode='Markdown')
+
+
 def scheduleRequest():
-    def requestToReg():
-        response = requests.get('https://api.cloudvps.reg.ru/v1/balance_data', headers={
-            "Authorization": REG_RU_API_KEY
-        })
-        data = json.loads(response.content)
-        print(f'Response: {data}')
-
-        balance = data["balance_data"]["balance"]
-        days_left = data["balance_data"]["days_left"]
-        monthly_cost = data["balance_data"]["monthly_cost"]
-
-        message = f'Облачный счет составляет *{balance} ₽*\n' \
-                  f'Этого хватит на *{days_left} дн.*\n' \
-                  f'Расход средств и прогноз потребления: *{monthly_cost} ₽/месяц*'
-
-        if len(chats.state) and days_left <= 5:
-            for chat in chats.state:
-                bot.send_message(chat, message, parse_mode='Markdown')
-
     print('Schedule is started')
-    schedule.every().day.at(SCHEDULE_TIME, REGION).do(requestToReg)
-
     while True:
         schedule.run_pending()
         time.sleep(1)
@@ -60,5 +59,10 @@ def start(message):
     bot.send_message(message.chat.id, 'Успешно отписались!\nЧтобы подписаться, жмякни на /subscribe')
 
 
-threading.Thread(target=scheduleRequest).start()
-bot.polling()
+if __name__ == "__main__":
+    # Create the job in schedule.
+    schedule.every().day.at(SCHEDULE_TIME, REGION).do(requestToReg)
+    # Thread
+    threading.Thread(target=scheduleRequest).start()
+    # Bot polling
+    bot.polling()
