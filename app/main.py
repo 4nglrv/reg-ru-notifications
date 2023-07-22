d4dbee23e95214dbe81e275a038d953b655c026c
Oleg Angulyarov <4nglrv@gmail.com>
2023-07-22 19:29:28 +0300

    Some changes

--- app/main.py
+++ app/main.py
@@ -1,15 +1,10 @@
 import threading
 import schedule
 import time
-import types
 
 from actions import reg_notification_msg_action
 from bot import bot, state
 from enviroments import SCHEDULE_TIME, REGION
-
-
-def start_actions_handler(state_context, bot_context):
-    reg_notification_msg_action(state=state_context, bot=bot_context)
 
 
 def schedule_request_handler():
@@ -21,7 +16,7 @@ def schedule_request_handler():
 
 if __name__ == "__main__":
     # Create the job in schedule
-    schedule.every().day.at(SCHEDULE_TIME, REGION).do(lambda: start_actions_handler(state, bot))
+    schedule.every().day.at(SCHEDULE_TIME, REGION).do(lambda: reg_notification_msg_action(state, bot))
     # schedule.every(5).seconds.do(start_actions_handler)
     # Thread
     threading.Thread(target=schedule_request_handler).start()
